2fe7bce2611b6d1a71f4f6ce453c1e9904b7b7e2
zylwqx <zylwqx@gmail.com>
2025-04-24 09:07:45 -0700

    Update game.py

--- game.py
+++ game.py
@@ -20,33 +20,50 @@ def main():
     running = True
 
     #game grid
-    grid = pygame.Rect(250, 150, 300, 600)
+    grid = pygame.Rect(200, 50, 400, 800)
+    #slot 1: 200
+    #slot 2: 240
+    #slot 3: 280
+    #slot 4: 320
+    #slot 5: 360
+    #slot 6: 400
+    #slot 7: 440
+    #slot 8: 480
+    #slot 9: 520
+    #slot 10: 560
+
 
     #different pieces
-    L = pygame.Rect(0, 0, 30, 120)
-    J = 1
-    I = pygame.Rect(0, 0, 30, 120)
-    T = 1
-    O = pygame.Rect(30, 0, 60, 60)
-    S = 1
-    Z = 1
+    square = pygame.Rect(360, 50, 40, 40)
+    square_speed = 40
 
     while running:
-        #player input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        keys = pygame.key.get_pressed()
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT]  or keys[pygame.K_a]:
+                square.x -= square_speed
+                if square.x <= 200:
+                    square.x = 200
+            if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+                square.x += square_speed
+                if square.x >= 560:
+                    square.x = 560
+            if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+                square.y += square_speed
+                if square.y >= 810:
+                    square.y = 810
 
         window.fill(BK)
 
         #draw player square
         pygame.draw.rect(window, W, grid)
         
-        pygame.draw.rect(window, PB, I)
-        pygame.draw.rect(window, PY, O)
+        #pygame.draw.rect(window, PB, I)
+        pygame.draw.rect(window, PY, square)
 
         pygame.display.update()
-        pygame.time.delay(20)
+        pygame.time.delay(50)
 
 main()
