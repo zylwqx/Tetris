5a1d0fb5be04f973253ede16f6f60735def57080
zylwqx <zylwqx@gmail.com>
2025-04-24 09:52:07 -0700

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import time
 
 def main():
     x = 800
@@ -32,38 +33,67 @@ def main():
     #slot 9: 520
     #slot 10: 560
 
+    tps = 20 
+    tps_timer = 0
 
     #different pieces
     square = pygame.Rect(360, 50, 40, 40)
     square_speed = 40
 
+    # Setup
+    gravity = 15
+    g_timer = gravity
+
+    p_timer 
+
+    curr_time = time.time()
+    prev_time = curr_time
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            
+
+        delta_t = curr_time - prev_time
+        prev_time = curr_time
+        curr_time = time.time()
+
+        tps_timer += delta_t
+        # Handle events
+        if tps_timer >= 1/tps:
+            tps_timer = 0
+
+            g_timer -= 1
+            if g_timer <= 0:
+                square.y += square_speed
+                g_timer = gravity
+
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT]  or keys[pygame.K_a]:
                 square.x -= square_speed
-                if square.x <= 200:
-                    square.x = 200
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
                 square.x += square_speed
-                if square.x >= 560:
-                    square.x = 560
-            if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                square.y += square_speed
-                if square.y >= 810:
-                    square.y = 810
+
+
+        # limits
+        if square.x <= 200:
+            square.x = 200
+        if square.x >= 560:
+            square.x = 560
+        if square.y >= 810:
+            square.y = 810
+        
+       
 
         window.fill(BK)
 
-        #draw player square
+        # draw square
         pygame.draw.rect(window, W, grid)
         
         #pygame.draw.rect(window, PB, I)
         pygame.draw.rect(window, PY, square)
 
         pygame.display.update()
-        pygame.time.delay(50)
 
 main()
